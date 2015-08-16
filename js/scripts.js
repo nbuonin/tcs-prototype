148e0fd70fca0389726accf5f992722552af69c0
Nick Buonincontri <nbuonin@gmail.com>
2015-08-16 00:09:11 -0400

    move subscribe form to nav area, write js toggle to show/hide sign-up form

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.nav-toggle button').click(
       function() { $('nav').toggleClass('active')}
     )
+
+  $('.email-subscribe').click(
+      function() { $('.subscribe').toggleClass('active')}
+    )
 });
