58da59cc2eef2bf8ab22bd82b2525dd67300b7c4
Nick Buonincontri <nbuonin@gmail.com>
2015-08-18 17:46:52 -0400

    add jquery ui, change markup for jquery accordion to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,21 @@ $(document).ready(function() {
       function() { $('nav').toggleClass('active')}
     )
 
-  $('.email-subscribe').click(
-      function() { $('.subscribe').toggleClass('active')}
-    )
+  // $('.email-subscribe').click(
+  //     function() { $('.subscribe').toggleClass('active')}
+  //   )
+
+  $(function() {
+    $( ".email-subscribe" ).accordion({
+        active: false,
+        collapsible: true
+    });
+  });
+  $(document).click(function(e) {
+    if (!$( ".email-subscribe" ).is(e.target) && !$(input[type=email] ).has(e.target).length) {
+        $('.email-subscribe').accordion('activate', -1);
+    }
+  });
 });
+
+
