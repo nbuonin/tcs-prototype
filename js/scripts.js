2969a88780b59d4dbe73f94d3e6c9ba783bd22d2
Nick Buonincontri <nbuonin@gmail.com>
2015-08-18 22:17:28 -0400

    js refactor, fix css for the widowed Olivers - see concert.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $('.nav-toggle button').click(
-      function() { $('nav').toggleClass('active')}
-    )
+  $('.nav-toggle button').on("click", function () {
+    $('nav').toggleClass('active');
+  });
 
   // $('.email-subscribe').click(
   //     function() { $('.subscribe').toggleClass('active')}
@@ -13,9 +13,12 @@ $(document).ready(function() {
         collapsible: true
     });
   });
-  $(document).click(function(e) {
-    if (!$( ".email-subscribe" ).is(e.target) && !$(input[type=email] ).has(e.target).length) {
-        $('.email-subscribe').accordion('activate', -1);
+  $(document).on("click",function(e) {
+    $('.email-subscribe').accordion('activate', -1);
+
+    if ($( ".email-subscribe" ).is(e.target) && !$("input[type=email]:focus")) {
+      window.alert("sometext");
+
     }
   });
 });
