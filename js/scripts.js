3c785f88874f648886a367ee5a9e588f2569fb78
Nick Buonincontri <nbuonin@gmail.com>
2015-08-30 17:01:12 -0400

    clean up scripts.js, better comment code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
+  //Creates accordion toggle for email signup form
   $('.nav-toggle button').on("click", function () {
     $('nav').toggleClass('active');
   });
-
-  // $('.email-subscribe').click(
-  //     function() { $('.subscribe').toggleClass('active')}
-  //   )
 
   $(function() {
     $( ".email-subscribe" ).accordion({
@@ -15,22 +12,12 @@ $(document).ready(function() {
   });
 
   $(document).on("click",function(e) {
-
-  //   if (!$( ".subscribe" ).is(e.target) && !$(".mc-field-group .email").is(":focus")) {
-  //     $('.email-subscribe').accordion("option", "active", false);
-  //     //window.alert("sometext");
-
-  //   }
-  // });
-  // $(" div .subscribe").hide();
-  // $(".email-subscribe").on("click", function () {
-  //   $(".email-subscribe .subscribe").toggle("blind");
-
     if (!$( ".email-subscribe" ).is(e.target) && !$( ".email-subscribe" ).has(e.target).length) {
         $(".email-subscribe").accordion("option", "active", false);
     }
   });
 
+  // modifies markup of profile page for RWD
   function profilePage () {
     if ($(window).width() > 850) {
       $(".profile-info-container").append($(".profile-bio"));
@@ -40,7 +27,6 @@ $(document).ready(function() {
 
   $(document).ready(profilePage);
   $(window).resize(profilePage);
-
 });
 
 
